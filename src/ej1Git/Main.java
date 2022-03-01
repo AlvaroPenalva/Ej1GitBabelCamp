c804add01272a28020fd82f81ffb7f8b2f83b6f8
alvaro <alvaro_penalva@hotmail.com>
2022-03-01 13:57:24 +0100

    Primer commit programa y clase Suma

---
+++ src/ej1Git/Main.java
@@ -0,0 +1,11 @@
+package ej1Git;
+
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main() {
+		
+	}
+	
+}
---
+++ src/ej1Git/Suma.java
@@ -0,0 +1,7 @@
+package ej1Git;
+public class Suma {
+	public static int acumulado;
+	public static int sumaAyB(int a, int b) {
+		return a + b;
+	}
+}
