3b2e9c57708269784a1b67b3221e1201d307ffff
alvaro <alvaro_penalva@hotmail.com>
2022-03-18 19:15:59 +0100

    añadidos atributos al proyecto ejercicio 9

--- Ej9BabelCamp/Ejercicio9/src/app/app.component.ts
+++ Ej9BabelCamp/Ejercicio9/src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Ejercicio9';
+
+  nombre = "Álvaro Penalva Alberca";
+
+  lenguajes:string = "C++, java, kotlin, HTML5, CSS, javascript, mongodb, SQL.";
+
+  correo:string = "alvaro_penalva@hotmail.com";
+
+  telefono:string = "659996293";
+
+  direccion:string = "Calle Fernán Caballero 26, 2ºF";
+
+  imagenPerfil:string = "assets/img/fotoCV.jpg";
+
 }
